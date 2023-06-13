bb2201668a33b9fb2656206e6417d5711ce09431
Srdjan <srki0505@gmail.com>
2023-06-13 04:17:17 +0200

    Service Drive tests

--- demo/demo/src/test/java/com/example/demo/service/DriveServiceTest.java
+++ demo/demo/src/test/java/com/example/demo/service/DriveServiceTest.java
@@ -329,4 +329,354 @@ public class DriveServiceTest {
         return "{\"code\":\"Ok\",\"distances\":[[0]],\"destinations\":[{\"hint\":\"w_GagSssboimAwAAkgQAAF4WAAC0QQAAlYiyRIVE30TyrghGjcjIRqYDAACSBAAAXhYAALRBAAAfGwAAHNixAj1KLwHthbIC7BMuAQMA7wMHTzZ8\",\"distance\":9953.393487108,\"name\":\"\",\"location\":[45.209628,19.876413]}],\"durations\":[[0]],\"sources\":[{\"hint\":\"w_GagSssboimAwAAkgQAAF4WAAC0QQAAlYiyRIVE30TyrghGjcjIRqYDAACSBAAAXhYAALRBAAAfGwAAHNixAj1KLwHthbIC7BMuAQMA7wMHTzZ8\",\"distance\":9953.393487108,\"name\":\"\",\"location\":[45.209628,19.876413]}]}";
     }
 
+    //declineDriveParticipation
+    @Test
+    public void shouldThrowNotPassengerDeclineParticipation() {
+
+        d.setPassengers(new HashSet<>());
+
+        doReturn(user).when(userService).getLoggedUser();
+        doReturn(Optional.of(d)).when(driveRepository).findById(any());
+
+        assertThrows(NotDrivePassengerException.class, () -> driveService.declineDriveParticipation(1L));
+
+    }
+
+    @Test
+    public void shouldDoNothingNotEveryoneAnsweredDeclineDriveParticipation() throws DriveNotFoundException, NotDrivePassengerException, TransactionIdDoesNotExistException {
+        User u = new User();
+        u.setEmail("passenger");
+        Passenger p1 = new Passenger();
+        p1.setPassengerEmail("passenger");
+        Passenger p2 = new Passenger();
+        p2.setPassengerEmail("passenger2");
+        p2.setContribution(DrivePassengerStatus.WAITING);
+        Set passengers = new HashSet<Passenger>();
+        passengers.add(p1);
+        passengers.add(p2);
+        d.setPassengers(passengers);
+
+
+        doReturn(u).when(userService).getLoggedUser();
+        doReturn(Optional.of(d)).when(driveRepository).findById(any());
+        doReturn(d).when(driveRepository).save(d);
+
+
+        Drive changed = driveService.declineDriveParticipation(1L);
+
+        for (Passenger pas : changed.getPassengers()) {
+            if (pas.getPassengerEmail().equals("passenger")) {
+                assertEquals(pas.getContribution(), DrivePassengerStatus.REJECTED);
+                assertEquals(pas.getPayment(), PaymentPassengerStatus.REJECTED);
+            }
+        }
+        assertNotEquals(changed.getDriveStatus(), DriveStatus.OWNER_PAYMENT_WAITING);
+        verify(driveRepository, times(1)).save(any());
+        verify(bankService, never()).declineTransaction(any(), any());
+
+    }
+
+    @Test
+    public void shouldDeclineDriveParticipation() throws DriveNotFoundException, NotDrivePassengerException, TransactionIdDoesNotExistException {
+        User u = new User();
+        u.setEmail("passenger");
+        Passenger p1 = new Passenger();
+        p1.setPassengerEmail("passenger");
+        Passenger p2 = new Passenger();
+        p2.setPassengerEmail("passenger2");
+        p2.setContribution(DrivePassengerStatus.ACCEPTED);
+        p2.setPayment(PaymentPassengerStatus.WAITING);
+        Set passengers = new HashSet<Passenger>();
+        passengers.add(p1);
+        passengers.add(p2);
+        d.setPassengers(passengers);
+
+
+        ClientsBankAccount clientsBankAccount = new ClientsBankAccount();
+        clientsBankAccount.setAccountNumber("111");
+        ClientsAccount owner = new ClientsAccount();
+        owner.setClientsBankAccount(clientsBankAccount);
+        owner.setUser(u);
+        d.setOwner(owner);
+        d.setPrice(500.0);
+
+        doReturn(u).when(userService).getLoggedUser();
+        doReturn(Optional.of(d)).when(driveRepository).findById(any());
+        doReturn(d).when(driveRepository).save(d);
+        doReturn(new BankTransaction()).when(bankService).declineTransaction(any(),any());
+        doNothing().when(notificationService).notifyOwnerDriveChanged(any(),any());
+
+        Drive changed = driveService.declineDriveParticipation(1L);
+
+        for (Passenger pas : changed.getPassengers()) {
+            if (pas.getPassengerEmail().equals("passenger")) {
+                assertEquals(pas.getContribution(), DrivePassengerStatus.REJECTED);
+                assertEquals(pas.getPayment(), PaymentPassengerStatus.REJECTED);
+            }
+        }
+        assertEquals(changed.getDriveStatus(), DriveStatus.OWNER_PAYMENT_WAITING);
+        verify(driveRepository, times(3)).save(any());
+        verify(bankService, times(1)).declineTransaction(any(), any());
+        verify(notificationService, times(1)).notifyOwnerDriveChanged(any(), any());
+
+    }
+    // continueWithDrive
+    @Test
+    public void shouldContinueWithDriveOwnerCanAfford() throws EmailNotFoundException {
+        ClientsAccount cl = new ClientsAccount();
+        cl.setUser(user);
+        d.setOwner(cl);
+        d.setOwnerDebit(500.0);
+        BankTransaction bankTransaction = new BankTransaction();
+        bankTransaction.setId(1L);
+
+        doReturn(true).when(userService).canAffordDrive(user.getEmail(), d.getOwnerDebit());
+        doReturn(bankTransaction).when(bankService).requestOwnerPayment(any());
+        doReturn(d).when(driveRepository).save(d);
+
+        Drive newDrive = driveService.continueWithDrive(d);
+
+        assertEquals(newDrive.getDriveStatus(), DriveStatus.OWNER_PAYMENT_WAITING);
+        assertEquals(newDrive.getOwnerTransactionId(), 1L);
+        verify(driveRepository, times(1)).save(d);
+
+    }
+
+    @Test
+    public void shouldNotContinueWithDriveOwnerCantAfford() throws EmailNotFoundException {
+        ClientsAccount cl = new ClientsAccount();
+        cl.setUser(user);
+        d.setOwner(cl);
+        d.setOwnerDebit(500.0);
+
+        doReturn(false).when(userService).canAffordDrive(user.getEmail(), d.getOwnerDebit());
+        doReturn(d).when(driveRepository).save(d);
+        doNothing().when(notificationService).paymentFailedDriveCanceledNotify(anyString());
+
+        Drive newDrive = driveService.continueWithDrive(d);
+
+        assertEquals(newDrive.getDriveStatus(), DriveStatus.DRIVE_FAILED);
+        verify(driveRepository, times(1)).save(d);
+
+    }
+    //acceptDriveParticipation
+    @Test
+    public void shouldThrowErrorAcceptDriveParticipation() {
+
+        d.setPassengers(new HashSet<>());
+
+        doReturn(user).when(userService).getLoggedUser();
+        doReturn(Optional.of(d)).when(driveRepository).findById(any());
+
+        assertThrows(NotDrivePassengerException.class, () -> driveService.acceptDriveParticipation(1L));
+
+    }
+
+    @Test
+    public void shouldDoNothingAcceptDriveParticipation() throws EmailNotFoundException, DriveNotFoundException, NotDrivePassengerException, URISyntaxException, IOException, TransactionIdDoesNotExistException, InterruptedException {
+        User user = new User();
+        user.setEmail("email@email.com");
+        Passenger p = new Passenger();
+        p.setPassengerEmail("email@email.com");
+        Passenger p1 = new Passenger();
+        p1.setPassengerEmail("nekidrugi.com");
+        p1.setContribution(DrivePassengerStatus.WAITING);
+        Set passengers = new HashSet<Passenger>();
+        passengers.add(p);
+        passengers.add(p1);
+        d.setPassengers(passengers);
+
+        doReturn(user).when(userService).getLoggedUser();
+        doReturn(Optional.of(d)).when(driveRepository).findById(any());
+        doReturn(d).when(driveRepository).save(d);
+
+        Drive newDrive = driveService.acceptDriveParticipation(1L);
+
+        for (Passenger pas : newDrive.getPassengers()) {
+            if (pas.getPassengerEmail().equals("email@email.com"))
+                assertEquals(pas.getContribution(), DrivePassengerStatus.ACCEPTED);
+        }
+
+        verify(driveRepository, times(1)).save(any());
+    }
+
+    @Test
+    public void acceptDriveParticipationEverybodyAcceptedNoSplitBill() throws EmailNotFoundException, DriveNotFoundException, NotDrivePassengerException, URISyntaxException, IOException, TransactionIdDoesNotExistException, InterruptedException {
+        User user = new User();
+        user.setEmail("email@email.com");
+        Passenger p = new Passenger();
+        p.setPassengerEmail("email@email.com");
+        Passenger p1 = new Passenger();
+        p1.setPassengerEmail("nekidrugi.com");
+        p1.setContribution(DrivePassengerStatus.ACCEPTED);
+        Set passengers = new HashSet<Passenger>();
+        passengers.add(p);
+        passengers.add(p1);
+        d.setPassengers(passengers);
+
+        d.setSplitBill(false);
+
+        doReturn(user).when(userService).getLoggedUser();
+        doReturn(Optional.of(d)).when(driveRepository).findById(any());
+        doReturn(d).when(driveRepository).save(d);
+        doReturn(d).when(driveService).findAvailableDriver(d);
+
+        Drive newDrive = driveService.acceptDriveParticipation(1L);
+
+        for (Passenger pas : newDrive.getPassengers()) {
+            if (pas.getPassengerEmail().equals("email@email.com"))
+                assertEquals(pas.getContribution(), DrivePassengerStatus.ACCEPTED);
+        }
+        assertEquals(newDrive.getDriveStatus(), DriveStatus.DRIVER_WAITING);
+        verify(driveRepository, times(2)).save(any());
+
+
+    }
+
+    @Test
+    public void acceptDriveParticipationEverybodyAcceptedSplitBillNoPoor() throws EmailNotFoundException, DriveNotFoundException, NotDrivePassengerException, URISyntaxException, IOException, TransactionIdDoesNotExistException, InterruptedException {
+        User user = new User();
+        user.setEmail("email@email.com");
+        Passenger p = new Passenger();
+        p.setPassengerEmail("email@email.com");
+        Passenger p1 = new Passenger();
+        p1.setPassengerEmail("nekidrugi.com");
+        p1.setContribution(DrivePassengerStatus.ACCEPTED);
+        Set passengers = new HashSet<Passenger>();
+        passengers.add(p);
+        passengers.add(p1);
+        d.setPassengers(passengers);
+
+        d.setSplitBill(true);
+
+        ClientsBankAccount bank1 = new ClientsBankAccount();
+        bank1.setBalance(1000.0);
+        ClientsAccount cl1 = new ClientsAccount();
+        cl1.setClientsBankAccount(bank1);
+        p.setDebit(500.0);
+        ClientsBankAccount bank2 = new ClientsBankAccount();
+        bank2.setBalance(1000.0);
+        ClientsAccount cl2 = new ClientsAccount();
+        cl2.setClientsBankAccount(bank2);
+        p1.setDebit(500.0);
+        p.setPayment(PaymentPassengerStatus.ACCEPTED);
+        p1.setPayment(PaymentPassengerStatus.ACCEPTED);
+        BankTransaction bankTransaction = new BankTransaction();
+        bankTransaction.setId(1L);
+
+        doReturn(bankTransaction).when(bankService).requestPassengerPayment(any(), any());
+        doReturn(cl1).when(userService).findClientsAccount(p.getPassengerEmail());
+        doReturn(cl2).when(userService).findClientsAccount(p1.getPassengerEmail());
+
+        doReturn(user).when(userService).getLoggedUser();
+        doReturn(Optional.of(d)).when(driveRepository).findById(any());
+        doReturn(d).when(driveRepository).save(d);
+        doReturn(d).when(driveService).findAvailableDriver(d);
+
+        Drive newDrive = driveService.acceptDriveParticipation(1L);
+
+        for (Passenger pas : newDrive.getPassengers()) {
+            assertEquals(pas.getTransactionId(), 1L);
+            if (pas.getPassengerEmail().equals("email@email.com"))
+                assertEquals(pas.getContribution(), DrivePassengerStatus.ACCEPTED);
+        }
+        assertEquals(newDrive.getDriveStatus(), DriveStatus.PAYMENT_WAITING);
+        verify(driveRepository, times(2)).save(any());
+
+
+    }
+
+    @Test
+    public void acceptDriveParticipationEverybodyAcceptedSplitBillPoor() throws EmailNotFoundException, DriveNotFoundException, NotDrivePassengerException, URISyntaxException, IOException, TransactionIdDoesNotExistException, InterruptedException {
+        User user = new User();
+        user.setEmail("email@email.com");
+        Passenger p = new Passenger();
+        p.setPassengerEmail("email@email.com");
+        Passenger p1 = new Passenger();
+        p1.setPassengerEmail("nekidrugi.com");
+        p1.setContribution(DrivePassengerStatus.ACCEPTED);
+        Set passengers = new HashSet<Passenger>();
+        passengers.add(p);
+        passengers.add(p1);
+        d.setPassengers(passengers);
+        d.setPrice(400.0);
+        d.setSplitBill(true);
+
+        ClientsBankAccount bank1 = new ClientsBankAccount();
+        bank1.setBalance(1000.0);
+        ClientsAccount cl1 = new ClientsAccount();
+        cl1.setClientsBankAccount(bank1);
+        p.setDebit(500.0);
+        ClientsBankAccount bank2 = new ClientsBankAccount();
+        bank2.setBalance(200.0);
+        ClientsAccount cl2 = new ClientsAccount();
+        cl2.setClientsBankAccount(bank2);
+        p1.setDebit(500.0);
+        p.setPayment(PaymentPassengerStatus.ACCEPTED);
+        p1.setPayment(PaymentPassengerStatus.ACCEPTED);
+        BankTransaction bankTransaction = new BankTransaction();
+        bankTransaction.setId(1L);
+        d.setOwner(cl1);
+
+        doReturn(bankTransaction).when(bankService).requestPassengerPayment(any(), any());
+        doReturn(cl1).when(userService).findClientsAccount(p.getPassengerEmail());
+        doReturn(cl2).when(userService).findClientsAccount(p1.getPassengerEmail());
+        doReturn(new BankTransaction()).when(bankService).declineTransaction(any(), any());
+
+        doReturn(bankTransaction).when(bankService).requestOwnerPayment(any());
+        doReturn(user).when(userService).getLoggedUser();
+        doReturn(Optional.of(d)).when(driveRepository).findById(any());
+        doReturn(d).when(driveRepository).save(d);
+        doReturn(d).when(driveService).findAvailableDriver(d);
+
+        Drive newDrive = driveService.acceptDriveParticipation(1L);
+
+        for (Passenger pas : newDrive.getPassengers()) {
+            if (pas.getPassengerEmail().equals("email@email.com"))
+                assertEquals(pas.getContribution(), DrivePassengerStatus.ACCEPTED);
+            if (pas.getPassengerEmail().equals("nekidrugi.com"))
+                assertEquals(pas.getPayment(), PaymentPassengerStatus.REJECTED);
+        }
+        verify(driveRepository, times(5)).save(any());
+    }
+
+
+    @Test
+    public void acceptDriveParticipationNotEverybodyAccepted() throws EmailNotFoundException, DriveNotFoundException, NotDrivePassengerException, URISyntaxException, IOException, TransactionIdDoesNotExistException, InterruptedException {
+        User user = new User();
+        user.setEmail("email@email.com");
+        Passenger p = new Passenger();
+        p.setPassengerEmail("email@email.com");
+        p.setPayment(PaymentPassengerStatus.ACCEPTED);
+        Passenger p1 = new Passenger();
+        p1.setPassengerEmail("nekidrugi.com");
+        p1.setContribution(DrivePassengerStatus.REJECTED);
+        p1.setPayment(PaymentPassengerStatus.ACCEPTED);
+        Set passengers = new HashSet<Passenger>();
+        passengers.add(p);
+        passengers.add(p1);
+        d.setPassengers(passengers);
+
+        d.setSplitBill(true);
+        d.setPrice(500.0);
+        ClientsBankAccount bank1 = new ClientsBankAccount();
+        bank1.setAccountNumber("aa");
+        ClientsAccount cl1 = new ClientsAccount();
+        cl1.setClientsBankAccount(bank1);
+        d.setOwnerTransactionId(1L);
+        d.setOwner(cl1);
+
+        doReturn(new BankTransaction()).when(bankService).declineTransaction(any(), any());
+        doNothing().when(notificationService).notifyOwnerDriveChanged(any(), any());
+        doReturn(user).when(userService).getLoggedUser();
+        doReturn(Optional.of(d)).when(driveRepository).findById(any());
+        doReturn(d).when(driveRepository).save(d);
+        doReturn(d).when(driveService).findAvailableDriver(d);
+
+        Drive newDrive = driveService.acceptDriveParticipation(1L);
+
+        assertEquals(newDrive.getDriveStatus(), DriveStatus.OWNER_PAYMENT_WAITING);
+        verify(driveRepository, times(3)).save(any());
+    }
+
 }
